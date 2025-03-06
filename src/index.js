5e62174d7a08c3ec4e6a5c7a6f69cc17883a6b83
JulianLopez2709 <judalomor27.95@gmail.com>
2025-03-05 21:16:10 -0500

    port dynamic added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from "./app.js"
 import { Server as WebsockerServer } from 'socket.io'
+import './config.js'
 import http from 'http';
 import { connect } from './db.js';
 import sockets from "./sockets.js";
@@ -8,9 +9,9 @@ connect()
 
 
 const server = http.createServer(app)
-
-const httpServer = server.listen(3000, () => {
-    console.log("Server is runnin on port 3000")
+const PORT = process.env.PORT || 3000;
+const httpServer = server.listen(PORT, () => {
+    console.log(`Server is runnin on port ${PORT}`)
 })
 
 
